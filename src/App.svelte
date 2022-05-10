4aeedcb852350cff8b4b45afc6f87205ffe686c9
vincjo <v.joseph3@gmail.com>
2022-05-10 18:17:22 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,44 @@
 <script>
-
-	const myapps = [
-		// { 
-		// 	name: `Boucle d'Air`, 
-		// 	title: 'Envie de Sortir ?',
-		// 	href: 'https://boucledair.fr/', 
-		// 	logo: 'boucledair.fr.svg', 
-		// 	color: '#079992', 
-		// 	language: 'french' 
-		// },
-		// { 
-		// 	name: `GeoFlux`, 
-		// 	title: `Technologies de l'information géographique`,
-		// 	href: 'https://geoflux.io/', 
-		// 	logo: 'geoflux.io.svg', 
-		// 	color: '#455a64', 
-		// 	language: 'french' 
-		// },
-		{ 
-			name: `svelte-simple-datatables`, 
-			title: 'A Datatable component for Svelte',
-			href: 'https://vincjo.fr/svelte-simple-datatables/', 
-			logo: 'svelte-simple-datatables.svg',
-			color: '#b71540', 
-			language: 'english' 
-		},
-	]
 </script>
 
 <main>
 	<h1>Bonjour / Hello</h1>
-	<!-- <p>Below is a list of sites and applications I maintain :</p> -->
-	{#each myapps as app}
 	<article>
-		<a href={app.href}>
-			<img alt="{app.name} logo" src="./{app.logo}"/>
+		<a href="https://vincjo.fr/svelte-simple-datatables/">
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+				<path fill="#b71540" d="M462.009,20.359H49.99C22.382,20.359,0,42.739,0,70.348v371.304c0,27.609,22.382,49.989,49.99,49.989
+					h412.019c27.609,0,49.991-22.382,49.991-49.989V70.348C512,42.739,489.618,20.359,462.009,20.359z"/>
+				<path style="fill:#fff;" d="M66.354,168.954c-5.137,0-9.316-4.178-9.316-9.315V73.06c0-5.136,4.178-9.314,9.316-9.314h387.59
+					c5.136,0,9.315,4.177,9.315,9.314v86.579c0,5.136-4.178,9.315-9.315,9.315H66.354z"/>
+				<g>
+					<rect x="57.038" y="215.793" style="fill:#fff;" width="170.928" height="80.41"/>
+					<rect x="57.038" y="352.186" style="fill:#fff;" width="170.928" height="80.41"/>
+					<rect x="283.01" y="215.793" style="fill:#fff;" width="170.928" height="80.41"/>
+					<rect x="283.01" y="352.186" style="fill:#fff;" width="170.928" height="80.41"/>
+				</g>
+				<path style="opacity:0.1;enable-background:new;" d="M104.338,441.652V70.348c0-27.609,22.382-49.989,49.99-49.989H49.99
+					C22.382,20.359,0,42.739,0,70.348v371.304c0,27.608,22.382,49.989,49.99,49.989h104.338
+					C126.719,491.641,104.338,469.26,104.338,441.652z"/>
+				</svg>
 			<div>
-				<h2 style="color:{app.color}">{app.name}</h2>
-				<h3>{app.title}</h3>
+				<h2 style:color={'#b71540'}>svelte-simple-datatables</h2>
+				<h3>A Datatable component for Svelte</h3>
 			</div>
 		</a>
 	</article>	
-	{/each}
+
+	<article>
+		<a href="https://vincjo.fr/svelte-data-handler/">
+			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="512" height="512" viewBox="0 0 24 24">
+				<path fill="#F66B0E" d="M21 2H3C1.9 2 1 2.9 1 4V20C1 21.1 1.9 22 3 22H21C22.1 22 23 21.1 23 20V4C23 2.9 22.1 2 21 2M21 20H3V6H21V20M6 8V18H10V16H8V10H10V8H6M16 16H14V18H18V8H14V10H16V16" />
+			</svg>
+			<div>
+				<h2 style:color={'#F66B0E'}>svelte-data-handler</h2>
+				<h3 style:color={'#205375'}>Easily manipulate an array of objects</h3>
+			</div>
+		</a>
+	</article>	
 </main>
 
 <style>
@@ -50,7 +47,7 @@
 	/* p{color:#757575;font-size:1em;} */
 	article{width:100%;max-width:512px;margin:24px auto;border:1px solid #eee;background:#fff;border-radius:8px;}
 	a{text-decoration:none;display:flex;padding:16px;}
-	img{float:left;width:104px;margin-right:24px;}
+	svg{float:left;width:104px;height:104px;margin-right:24px;}
 	h2{font-weight:400;text-align:left;margin:0 0 0 0;font-size:2rem;}
 	h3{font-weight:300;color:#455a64;text-align:left;margin:0 0 4px 0}
 	@media (max-width: 640px) {
@@ -58,7 +55,7 @@
 		article{max-width:90%;margin:16px auto;}
 		h1{font-size:1.8em;}
 		/* p{font-size:.9em;padding:4% 0;} */
-		img{width:64px;margin-right:16px;}
+		svg{width:64px;height:64px;margin-right:16px;}
 		a{padding:8px}
 		h2{font-size:1.1em;margin:0 0 4px 0}
 		h3{font-size:1em;}
