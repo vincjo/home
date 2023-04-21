3ffa95c67b9ba8b14638eebcfaf862383541c451
vincjo <v.joseph3@gmail.com>
2023-04-21 15:23:47 +0200

    update color

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
         <a href="https://vincjo.fr/datatables">
             <svg class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                <path fill="#b71540" d="M462.009,20.359H49.99C22.382,20.359,0,42.739,0,70.348v371.304c0,27.609,22.382,49.989,49.99,49.989
+                <path fill="#c2185b" d="M462.009,20.359H49.99C22.382,20.359,0,42.739,0,70.348v371.304c0,27.609,22.382,49.989,49.99,49.989
                     h412.019c27.609,0,49.991-22.382,49.991-49.989V70.348C512,42.739,489.618,20.359,462.009,20.359z"/>
                 <path style="fill:#fff;" d="M66.354,168.954c-5.137,0-9.316-4.178-9.316-9.315V73.06c0-5.136,4.178-9.314,9.316-9.314h387.59
                     c5.136,0,9.315,4.177,9.315,9.314v86.579c0,5.136-4.178,9.315-9.315,9.315H66.354z"/>
@@ -22,7 +22,7 @@
                     C126.719,491.641,104.338,469.26,104.338,441.652z"/>
                 </svg>
             <div>
-                <h2 style:color={'#b71540'}>svelte simple datatables</h2>
+                <h2 style:color={'#c2185b'}>svelte simple datatables</h2>
                 <h3>A toolkit for creating datatable components with Svelte</h3>
             </div>
         </a>
