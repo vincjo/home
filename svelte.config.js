570075abadf6fc440b6fa94c67aa663152c21fd8
vincjo <v.joseph3@gmail.com>
2024-09-29 23:02:48 +0200

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-node'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess(),
 	kit: {
 		adapter: adapter({ out: 'build' }),
 	}
